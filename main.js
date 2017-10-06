80d99901b8e1013d4e054c463e2ff5ddd427ab77
LogicalShark <marcusa3.14@gmail.com>
2017-10-06 14:30:02 -0400

    ?

---
+++ main.js
@@ -0,0 +1,192 @@
+function createTable(input, order=4) 
+{
+    var table = []
+    //Make the index table
+    for (var i = 0; i<len(input); i++)
+    {
+        var sub = input.substr(i, order);
+        table[sub] = [];
+    }              
+    //Count the following strings for each string
+    for (var j = 0; j<(len(input) - order); j++) 
+    {
+        var index = input.substr(j, order);
+        var next = input.substr(j+order, order);
+        if(table[index][next]!=undefined)
+            table[index][next]++;
+        else
+            table[index][next] = 1;
+    }
+    return table;
+}
+function createText(first=' ', length=2000, table, order=4) 
+{
+    var chars = first;
+    if(first ==' ')
+    {
+        chars = table[Math.floor(Math.random()*items.length)];
+    }
+    var output = chars;
+    for (var k = 0; k<(length/order); k++) 
+    {
+        newchars = createNextChars(table[chars]);
+        if(newchars != null)
+        {
+            chars = newchars;
+            output += newchars;
+        } 
+        else 
+        {       
+            chars = table[Math.floor(Math.random()*items.length)];
+        }
+    }
+    return output;
+}
+function sum(a,b)
+{
+    return a+b;
+}
+function createNextChars(array) 
+{
+    if(array==undefined)
+        return null;
+    var total = total.reduce(sum)
+    var rand  = Math.floor(Math.random() * (total - 1)) + 1;
+    for (var l = 0; l<array.length; l++) 
+    {
+        var weight = array[l];
+        if (rand <= weight)
+            return item;
+        rand -= weight;
+    }
+}
+function getPhonemes(i)
+{
+    //Ensure correct format, remove punctuation
+    input = i.replace(/\n/g, '');
+
+    var phonA = ""
+    var re = /<tt>.*<\\tt><\/div>/;
+    var iwords = input.split(" ");
+    for (var i = 0, len = iwords.length; i < len; i++)
+    {
+        var w = iwords[i];
+        //do xhhtp request stuff
+        // $("#data").load("http://www.speech.cs.cmu.edu/cgi-bin/cmudict?in="+w);
+        var out = re.exec(document.getElementyById("data").value);
+        console.log(out);
+        var final = out.substring(4,out.length-14);
+        phonA+=final;
+    }
+    return phonA;
+}
+function generate() 
+{
+    //API Keys
+    //(top tier security 👌)
+    //103869fa
+    //79e16dba710549c398ebc4bec069d0dd
+
+    //Read inputs
+    var input = document.getElementyById("input").value();
+    var length = document.getElementyById("length").value();
+    var order = document.getElementyById("order").value();
+    //Get phonemes
+    var phonA = getPhonemes(input);            
+    //Translate to IPA
+    var phonI = transform(phonA);
+    //Generate output
+    var t = createTable(phonI, order);
+    var out = createText(' ', length, t, order);
+    console.log(out);
+    //Get audio files
+    var audios = getAudioFiles(out);
+    playAudio(audios);
+}
+function getAudioFiles(out)
+{
+    audios = []
+    var j = 0;
+    var len = out.length;
+    while(j<len)
+    {
+        //1 char left
+        var c = out[j]
+        if((len - j)==1)
+        {
+            audios.append(c);
+            j+=1;
+            continue;
+        }
+        //2 chars left
+        var c2 = c + out[j+1];
+        if((len - j)==2)
+        {
+            if(getAFile(c2))
+            {
+                audios.append(c2);
+                j+=2;
+            }
+            else
+            {
+                audios.append(c);
+                j+=1;
+            }
+            continue;
+        }
+        //Default
+        var c3 = c2 + out[j+2];
+        if(hasAFile(c3))
+        {
+            audios.append(c3)
+            j+=3;
+        }
+        else if(getAFile(c2))
+        {
+            audios.append(c2);
+            j+=2;
+        }
+        else
+        {
+            audios.append(c);
+            j+=1;
+        }
+    }
+}
+function playAudio(audios)
+{
+    for(var i = 0; i<audios.length; i++)
+    {
+        var p = audios[i];
+        var a = new Audio(audioFiles[p]);
+        a.play();
+    }
+}
+function hasAFile(chars)
+{
+    var audioFiles = []
+    if(audioFiles.includes(chars))
+        return true
+    return false;
+}
+function transform(phonA)
+{
+    //ARPABET and IPA phonemes
+    var arpCons = ["P","B","EM","T","D","EN","CH","DH","DX","EL","EN","F","G","H","JH","K","L","M","N","NG","NX","Q","R","S","SH","T","TH","V","w","WH","Y","Z","ZH"];
+    var ipaCons = ["p","b","m̩", "t","d","n", "tʃ","ð", "ɾ", "l̩", "n̩", "f","g","h","dʒ","k","l","m","n","ŋ", "ɾ̃", "ʔ","ɹ","s","ʃ", "t","θ", "v","W","ʍ", "j","z","ʒ" ];
+    var arpVow = ["AA", "AE", "AH", "AO", "AW", "AX", "AXR", "AY", "EH", "ER", "EY", "IH", "IX", "IY", "OW", "OY", "UH", "UW", "UX"];
+    var ipaVow = ["ɑ", "æ", "ʌ", "ɔ", "aʊ", "ə", "ɚ", "aɪ", "ɛ", "ɝ", "eɪ", "ɪ", "ɨ", "i", "oʊ", "ɔɪ", "ʊ", "u", "ʉ"];
+
+    var out = ""
+    for(int i = 0; i<phonA.length; i++)
+    {
+        var el = phonA[i];
+        if(arpaCons.includes(el))
+            out += ipaCons[arpaCons.indexOf(el)];
+        else if(arpaVow.includes(el))
+            out += ipaVow[arpaVow.indexOf(el)];
+        else
+            out+=el;
+    }
+    return out;
+}
