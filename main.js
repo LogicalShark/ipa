3071acd2257ac4eed542f0e22bc2e4b25309072e
LogicalShark <marcusa3.14@gmail.com>
2017-10-06 23:49:34 -0400

    please update

--- main.js
+++ main.js
@@ -232,13 +232,3 @@ function readFile()
         }
     }
     rawFile.send(null);*/
-/*function makeHttpObject() {
-  try {return new XMLHttpRequest();}
-  catch (error) {}
-  try {return new ActiveXObject("Msxml2.XMLHTTP");}
-  catch (error) {}
-  try {return new ActiveXObject("Microsoft.XMLHTTP");}
-  catch (error) {}
-
-  throw new Error("Could not create HTTP request object.");
-}*/
