88b303b6b69e2c341d5e180e3f52b51010b9654c
LogicalShark <marcusa3.14@gmail.com>
2017-10-09 16:02:57 -0400

    database

--- main.js
+++ main.js
@@ -101,12 +101,16 @@ function getPhonemes(i)
     input = i.replace(/\*/g, '');
 
     var phonA = [];
-
-    var dict = (document.getElementById("data").innerHTML).split("\n");
-    var alphabetIndicies = {A:127,B:7362,C:17044,D:27737,E:35475,F:40208,G:45422,H:51129,I:57571,J:60959,
-                            K:62628,L:66784,M:72292,N:81821,O:85026,P:88008,Q:96254,R:96710,S:104038,
-                            T:118031,U:123666,V:125466,W:127796,X:132182,Y:132261,Z:132989,z:133906};
     var iwords = input.split(" ");
+
+    var userId = firebase.auth().currentUser.uid;
+    var dict = firebase.database().ref('/ARP/' + userId).once('value').then(function(snapshot) {
+    var username = snapshot.val();
+    });
+    // var dict = (document.getElementById("data").innerHTML).split("\n");
+    // var alphabetIndicies = {A:127,B:7362,C:17044,D:27737,E:35475,F:40208,G:45422,H:51129,I:57571,J:60959,
+    //                         K:62628,L:66784,M:72292,N:81821,O:85026,P:88008,Q:96254,R:96710,S:104038,
+    //                         T:118031,U:123666,V:125466,W:127796,X:132182,Y:132261,Z:132989,z:133906};
     for (var i = 0, len = iwords.length; i < len; i++)
     {
         var w = iwords[i];
@@ -114,16 +118,24 @@ function getPhonemes(i)
         w = w.replace(/\)/,"\\)");
         w = w.replace(/\[/,"\\[");
         w = w.replace(/\]/,"\\]");
-        var re = new RegExp("^"+w+" ", "g");
-        for(var j = alphabetIndicies[w[0]]; j<alphabetIndicies[nextLetter(w[0])]; j++)
-        {
-            var wordline = dict[j];
-            if(wordline.match(re))
+        w = w+" ";
+        database.ref('/ARP/'+w).once('value').then(function(snapshot) {
+            var p = snapshot.val().phonemes;
+            if(username)
             {
-                phonA.push(wordline.replace(re,""))
-                break;
+                phonA.push(p);
             }
-        }
+        });
+        // var re = new RegExp("^"+w+" ", "g");
+        // for(var j = alphabetIndicies[w[0]]; j<alphabetIndicies[nextLetter(w[0])]; j++)
+        // {
+        //     var wordline = dict[j];
+        //     if(wordline.match(re))
+        //     {
+        //         phonA.push(wordline.replace(re,""))
+        //         break;
+        //     }
+        // }
         phonA.push(" ");
     }
     return phonA;
