93629d9475649f913fac53cd802f4ca41a836186
LogicalShark <marcusa3.14@gmail.com>
2017-10-11 19:25:39 -0400

    proper length

--- main.js
+++ main.js
@@ -42,7 +42,7 @@ function createText(start,length,table,order,size)
         chars = createNextChars(table[keys[Math.floor(Math.random()*size)]]);
     }
     var output = chars;
-    for (var k = 0; k<(length/order); k++) 
+    for (var k = 0; k<length; k+=order)
     {
         newchars = createNextChars(table[chars]);
         if(newchars!=undefined && newchars.length>0)
