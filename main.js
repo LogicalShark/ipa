13fbb1168f88357688d695d3ad670783ae2380f9
Marcus Alder <marcusa3.14@gmail.com>
2018-01-09 15:30:08 -0500

    Update main.js

--- main.js
+++ main.js
@@ -203,7 +203,7 @@ function transform(phonA)
 }
 
 //---------------------------------Get pronunciation data---------------------------------
-/*
+/* will finish later probably
 function writeData()
 {
     var database = firebase.database();
@@ -359,9 +359,15 @@ function generateText(input,length,order,start)
 function switchMode()
 {
     if document.getElementById("header").innerHTML == "Text Generator"
+    {
         document.getElementById("header").innerHTML = "Phoneme Generator";
+        document.getElementById("title").innerHTML = "Phoneme Generator";
+    }
     else
+    {
         document.getElementById("header").innerHTML = "Text Generator";
+        document.getElementById("title").innerHTML = "Phoneme Generator";
+    }
 function generate()
 {
     var input = (document.getElementById("input")).value;
